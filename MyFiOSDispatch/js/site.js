889ddb12f642ae3102f1e5c8847be92a4cc58d4a
memu-mani <memumani@gmail.com>
2015-11-01 05:10:06 -0800

    tewst

--- MyFiOSDispatch/js/site.js
+++ MyFiOSDispatch/js/site.js
@@ -139,9 +139,9 @@ $(document).ready(function () {
     $('#requestNow').click(function ()
     {
     //debugger;
-          var result = window.alert("Please confirm your request");
+          window.alert("Please confirm your request");
 
-           var result = window.confirm("Please confirm your request");
+           //var result = window.confirm("Please confirm your request");
 
           return false;
     });
